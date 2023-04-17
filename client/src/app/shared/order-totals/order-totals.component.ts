ef0b404871aaf19e0b088218982341124428ff8e
Kenffy <voufomarius@yahoo.fr>
2023-04-17 14:28:37 +0200

    End of section 14

---
+++ client/src/app/shared/order-totals/order-totals.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { BasketService } from 'src/app/basket/basket.service';
+
+@Component({
+  selector: 'app-order-totals',
+  templateUrl: './order-totals.component.html',
+  styleUrls: ['./order-totals.component.scss'],
+})
+export class OrderTotalsComponent implements OnInit {
+  constructor(public basketService: BasketService) {}
+
+  ngOnInit(): void {}
+}
